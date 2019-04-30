e941b37eec3a9141c5b57bcae27d010b5fdadc55
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-04-30 15:22:36 +0100

    Add some more info to get_git_word_count.

--- bin/get_git_word_count.py
+++ bin/get_git_word_count.py
@@ -5,13 +5,18 @@ import sys
 import subprocess as sp
 import re
 import datetime as dt
+from collections import defaultdict
 
 import matplotlib
-matplotlib.use('Qt4Agg')
 import matplotlib.pyplot as plt
 
-def get_word_count():
-    return int(re.search('Words in text: (?P<word_count>\d*)', sp.check_output('texcount main.tex'.split()).decode())['word_count'])
+def get_summary_info():
+    texcount_output = sp.check_output('texcount main.tex'.split()).decode()
+    print(texcount_output)
+    words = int(re.search('Words in text: (?P<words>\d*)', texcount_output)['words'])
+    caption_words = int(re.search('Words outside text \(captions, etc.\): (?P<caption_words>\d*)', texcount_output)['caption_words'])
+    floats = int(re.search('Number of floats/tables/figures: (?P<floats>\d*)', texcount_output)['floats'])
+    return {'words': words, 'caption_words': caption_words, 'floats': floats}
 
 def parse_git_datetime(s):
     return dt.datetime.strptime(s, '%Y-%m-%d %H:%M:%S %z')
@@ -22,19 +27,27 @@ if __name__ == '__main__':
     os.chdir(sys.argv[1])
 
     assert sp.check_output('git branch'.split()).decode().strip() == '* master'
-
     rev_list = sp.check_output('git rev-list master'.split()).decode().split('\n')
 
     dates = []
-    word_count = []
+    counts = defaultdict(list)
     for rev in rev_list:
         sp.call('git checkout {}'.format(rev).split())
-        word_count.append(get_word_count())
+        summary_info = get_summary_info()
+        for k, v in summary_info.items():
+            counts[k].append(v)
+
         dates.append(parse_git_datetime(sp.check_output('git show -s --format=%ci'.split()).decode().strip()))
 
-    plt.plot(dates[::-1], word_count[::-1])
+    for k, v in counts.items():
+        plt.figure(k)
+        plt.title(k)
+        plt.plot(v[::-1])
+        # plt.plot(dates[::-1], v[::-1])
+        # plt.xticks(rotation=90)
+        plt.tight_layout()
+
     plt.show()
     sp.call('git checkout master'.split())
 
     os.chdir(orig_dir)
-
