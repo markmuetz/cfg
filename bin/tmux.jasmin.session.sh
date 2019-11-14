276dcf9e0e7656ab0d9f67c3b108d9fadab8bbef
Mark Muetzelfeldt <markmuetz@gmail.com>
2019-11-14 12:06:06 +0000

    Add tmux.jasmin.session.sh
    Bash script that either connects to Jasmin session or
    creates a new session with different windows/panes corresponding
    to different activities.

---
+++ bin/tmux.jasmin.session.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function has-session {
+  tmux has-session -t Jasmin 2>/dev/null
+}
+
+if has-session ; then
+    echo "Session Jasmin already exists, attaching"
+    sleep 1
+    tmux attach -t Jasmin
+else
+    echo "Creating new Jasmin session"
+    tmux new-session -d -s 'Jasmin' -n jobs
+    tmux split-window -h
+    tmux select-pane -t 0
+    tmux split-window 
+
+    tmux new-window -n dev
+    tmux split-window -h
+
+    tmux new-window -n ctrl
+    tmux split-window -h
+
+    tmux new-window -n data
+    tmux split-window -h
+
+    tmux select-window -t 2
+
+    # Allow shells enough time to initialize.
+    sleep 10
+    tmux send-keys -t Jasmin:0.0 'watch "bjobs 2>&1"' C-m
+    tmux send-keys -t Jasmin:0.1 'watch -n10 "bjobs 2>&1|grep RUN|wc && bjobs 2>&1|grep PEND|wc"' C-m
+
+    tmux send-keys -t Jasmin:1.0 'cd $WCOSMIC/mmuetz/projects/cosmic' C-m
+    tmux send-keys -t Jasmin:1.1 'cd $WCOSMIC/mmuetz/cosmic_ctrl' C-m
+
+    tmux send-keys -t Jasmin:2.0 'cd $WCOSMIC/mmuetz/cosmic_ctrl && conda activate cosmic_env' C-m
+    tmux send-keys -t Jasmin:2.1 'cd $WCOSMIC/mmuetz/cosmic_ctrl' C-m
+
+    tmux -2 attach-session -d
+fi
